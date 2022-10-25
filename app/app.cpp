864fb25862b3ae8554059c825aed9a4f8464f9e8
Filip Les <ketvok@gmail.com>
2022-10-25 11:11:42 +0200

    Svi testovi prolaze.

--- app/app.cpp
+++ app/app.cpp
@@ -2,11 +2,60 @@
 
 #include <format>
 
+#include <sstream>
+
 namespace vsite::oop::p1 {
 
-/*
-* function implementations
-*/
+	/*
+	* function implementations
+	*/
 
+	double f1(int a, int b) {
+		return pow(a, b);
+	}
+
+	bool f2(bool a) {
+		return a ? false : true;
+	}
+
+	int f3(int a, int b, int c, int d) {  // Default values are specified only in function declaration!
+		return a + b + c + d;
+	}
+
+	std::string f4(int a) {
+		return "int";
+	}
+
+	std::string f4(bool a) {
+		return "bool";
+	}
+
+	std::string f4(double a) {
+		return "double";
+	}
+
+	bool f5(std::string& s, int number) {
+		if (number > 0) {
+			int last_digit = number % 10;
+			std::string last_character = std::to_string(last_digit);  // Conversion of int to std::string
+			s += last_character;  // Concatenation
+			return true;
+		}
+		return false;
+	}
+
+	/*
+		Alternative from lecture:
+		bool f5(std::string& s, int number){
+			if (n <= 0)
+				return false;
+			s += std::to_string(number % 10);
+		//	ili s += std::format("{}", number % 10);
+			return true;
+	*/
+
+	void f6(int a, int b, std::stringstream& ss) {
+		ss << a << "\n" << b;
+	}
 
 } // namespace
--- app/app.h
+++ app/app.h
@@ -2,12 +2,45 @@
 
 #include <string>
 #include <iostream>
+#include <sstream>
 
-namespace vsite::oop::p1 
+namespace vsite::oop::p1
 {
-/*  
-* function declarations
-*/
+	/*
+	* function declarations
+	*/
 
+	double f1(int a, int b);
+
+	bool f2(bool);
+	/*
+	* 	auto ret = f2(1 == 1);
+		Assert::AreEqual(1ull, sizeof(ret));
+
+		Ovo u test.cpp-u služi tome da se osigura da koristimo bool, a ne int kao u c-u.
+		sizeof(ret) vraæa unsigned long long.
+		1ull konvertira integer 1 u unsigned long long, da bi mogli raditi
+		usporedbu s varijablom ret.
+	*/
+
+	int f3(int a = 1, int b = 2, int c = 3, int d = 4);
+
+	std::string f4(int a);
+
+	std::string f4(bool a);
+
+	std::string f4(double a);
+
+	/*
+		.c_str() konvertira std::string u char* ("c-ovski string") da bi mogli
+		raditi usporedbu s char* stringovima koji su navedeni kao povratne
+		vrijednosti u testu.
+		U nekim drugim test frameworksima to se ne treba, ali ovaj zahtjeva da
+		su te dvije stvari istog tipa.
+	*/
+
+	bool f5(std::string& s, int number);
+
+	void f6(int a, int b, std::stringstream& ss);
 
 }
