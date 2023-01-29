87c2007f53d5523cc154862e68b824e0ef3fb58b
Ivan Selendic <ivan.selendic@gmail.com>
2023-01-30 00:58:17 +0100

    Tests passed

--- app/app.cpp
+++ app/app.cpp
@@ -2,11 +2,49 @@
 
 #include <format>
 
+
+
 namespace vsite::oop::p1 {
 
-/*
-* function implementations
-*/
 
+	double f1(int x, int y) {
+		return pow(x, y);
+	}
+	bool f2(bool a) {
+		return !a;
+	}
+
+	int f3(int x, int y, int z, int k) {
+		return x + y + z + k;
+	}
+
+	std::string f4(int x) {
+		return "int";
+
+	}
+
+
+	std::string f4(bool x) {
+		return "bool";
+	}
+
+	std::string f4(double x) {
+		return "double";
+	}
+
+
+	bool f5(std::string& s, int x) {
+		if (x <= 0) {
+			return false;
+
+		}
+		s += std::to_string(x % 10);
+		return true;
+	}
+
+	void f6(int a, int b, std::ostream& ss) {
+		ss << a << "\n" << b;
+
+	}
 
 } // namespace
--- app/app.h
+++ app/app.h
@@ -3,11 +3,22 @@
 #include <string>
 #include <iostream>
 
-namespace vsite::oop::p1 
+namespace vsite::oop::p1
 {
-/*  
-* function declarations
-*/
+	double f1(int x, int y);
 
+	bool f2(bool a);
+
+	int f3(int x = 1, int y = 2, int z = 3, int k = 4);
+
+	std::string f4(int x);
+
+	std::string f4(bool x);
+
+	std::string f4(double x);
+
+	bool f5(std::string& s, int x);
+
+	void f6(int a, int b, std::ostream& ss);
 
 }
