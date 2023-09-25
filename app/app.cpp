ebeb3894e14727dfe6388a9efc435df0dae39868
Nenad Čaklović <nenad.caklovic@reversinglabs.com>
2023-09-25 20:03:05 +0200

    p01 solved

--- app/app.cpp
+++ app/app.cpp
@@ -8,5 +8,32 @@ namespace vsite::oop::p1 {
 * function implementations
 */
 
+    double f1(int a, int b) {
+        return pow(a, b);
+    }
+
+    bool f2(bool b) {
+        return !b;
+    }
+
+    int f3(int a, int b, int c, int d) {
+        return a + b + c + d;
+    }
+
+    std::string f4(int) { return "int"; }
+    std::string f4(bool) { return "bool"; }
+    std::string f4(double) { return "double"; }
+
+    bool f5(std::string& s, int n) {
+        if (n <= 0)
+            return false;
+        s += std::to_string(n % 10);
+        //    s += std::format("{}", n%10);
+        return true;
+    }
+
+    void f6(int a, int b, std::ostream& os) {
+        os << a << '\n' << b;
+    }
 
 } // namespace
--- app/app.h
+++ app/app.h
@@ -5,9 +5,15 @@
 
 namespace vsite::oop::p1 
 {
-/*  
-* function declarations
-*/
+	double f1(int a, int b);
+	bool f2(bool b);
+	int f3(int a = 1, int b = 2, int c = 3, int d = 4);
 
+	std::string f4(int);
+	std::string f4(bool);
+	std::string f4(double);
 
+	bool f5(std::string& s, int n);
+
+	void f6(int a, int b, std::ostream& os);
 }
