c7728284778eb7678bccaa9107ca220d38825015
Josip Grabušić <jograbus@vsite.hr>
2023-10-09 21:20:28 +0200

    Promjena prve tri funkcije.

--- app/app.cpp
+++ app/app.cpp
@@ -4,9 +4,17 @@
 
 namespace vsite::oop::p1 {
 
-/*
-* function implementations
-*/
+	double f1(int a, int b) {
+		return pow(a, b);
+	}
 
+	bool f2(bool b) {
+		return (!b);
 
-} // namespace
+	}
+
+	int f3(int a, int b, int c, int d) {
+		return (a + b + c + d);
+	}
+
+} 
--- app/app.h
+++ app/app.h
@@ -5,9 +5,9 @@
 
 namespace vsite::oop::p1 
 {
-/*  
-* function declarations
-*/
+	double f1(int a, int b);
 
+	bool f2(bool b);
 
+	int f3(int a = 1, int b = 2, int c = 3, int d = 4);
 }
