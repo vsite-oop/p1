50ab87a052e58845e42c1f5ed4ed74b86829ea31
AnMal <junkie_@msn.com>
2023-10-10 11:04:27 +0200

    Created missing functions.

--- app/app.cpp
+++ app/app.cpp
@@ -1,6 +1,10 @@
 #include "app.h"
 
 #include <format>
+#include <cmath>
+#include <string>
+#include <sstream>
+#include <iostream>
 
 namespace vsite::oop::p1 {
 
@@ -8,5 +12,43 @@ namespace vsite::oop::p1 {
 * function implementations
 */
 
+    double f1(int x, int y) {
+        return pow(x, y);
+    }
+
+    bool f2(bool x) {
+        return !x;
+    };
+
+    int f3(int a, int b, int c, int d) {
+        return a + b + c + d;
+    }
+
+    std::string f4(int a) {
+        return typeid(a).name();
+    }
+
+    std::string f4(bool a) {
+        return typeid(a).name();
+    }
+
+    std::string f4(double a) {
+        return typeid(a).name();
+    }
+
+    bool f5(std::string& s, int x) {
+        if (x > 0) {
+            int digit = x % 10;
+            s = s + std::to_string(digit);
+
+            return true;
+        }
+        return false;
+    }
+
+
+    void f6(int x, int y, std::stringstream& ss) {
+        ss << x << "\n" << y;
+    }
 
 } // namespace
--- app/app.h
+++ app/app.h
@@ -2,6 +2,8 @@
 
 #include <string>
 #include <iostream>
+#include <string>
+#include <sstream>
 
 namespace vsite::oop::p1 
 {
@@ -9,5 +11,16 @@ namespace vsite::oop::p1
 * function declarations
 */
 
+    double f1(int x, int y);
+    bool f2(bool x);
+    int f3(int a = 1, int b = 2, int c = 3, int d = 4);
+
+    std::string f4(int a);
+    std::string f4(bool a);
+    std::string f4(double a);
+
+    bool f5(std::string& s, int x);
+
+    void f6(int x, int y, std::stringstream& ss);
 
 }
