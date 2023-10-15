9e9f80f5b88432306352090a18e26661fe7cdc03
Josip Grabušić <jograbus@vsite.hr>
2023-10-15 15:34:33 +0200

    p01 solved

--- app/app.cpp
+++ app/app.cpp
@@ -17,4 +17,31 @@ namespace vsite::oop::p1 {
 		return (a + b + c + d);
 	}
 
-} 
+
+	std::string f4(int b) {
+		return "int";
+	}
+
+	std::string f4(bool b) {
+		return "bool";
+	}
+
+	std::string f4(double b) {
+		return "double";
+	}
+
+
+	bool f5(std::string& s, int n) {
+		if (n > 0) {
+			int znamenka = n % 10;
+			s += std::to_string(znamenka);
+			return true;
+		}
+		return false;
+	}
+
+
+	void f6(int a, int b, std::ostream& ss) {
+		ss << a << '\n' << b;
+	}
+}
--- app/app.h
+++ app/app.h
@@ -10,4 +10,12 @@ namespace vsite::oop::p1
 	bool f2(bool b);
 
 	int f3(int a = 1, int b = 2, int c = 3, int d = 4);
+
+	std::string f4(int b);
+	std::string f4(bool b);
+	std::string f4(double b);
+
+	bool f5(std::string& s, int n);
+
+	void f6(int x, int y, std::ostream& ss);
 }
