34b861c5c0512b6229c325ec766f6f93631a5d0f
KriZa96 <kriza9605@gmail.com>
2024-10-16 15:49:10 +0200

    Rijesenje probne vijezbe

--- app/app.cpp
+++ app/app.cpp
@@ -1,12 +1,58 @@
 #include "app.h"
 
 #include <format>
+#include <cmath>
+#include <complex>
+#include <numeric>
 
 namespace vsite::oop::p1 {
 
-/*
-* function implementations
-*/
+	/*
+	* function implementations
+	*/
+	double f1(const int x, const int y)
+	{
+		return std::pow(static_cast<int>(x), static_cast<double>(y));
+	}
 
+	bool f2(const bool expression)
+	{
+		return not expression;
+	}
+
+
+	int f3(const int a, const int b, const int c, const int d)
+	{
+		return a + b + c + d;
+	}
+
+	std::string f4(const bool element)
+	{
+		return "bool";
+	}
+	std::string f4(const int element)
+	{
+		return "int";
+	}
+	std::string f4(const double element)
+	{
+		return "double";
+	}
+
+	bool f5(std::string& s, const int num)
+	{
+		if (num <= 0)
+		{
+			return false;
+		}
+
+		s += std::to_string(num % 10);
+		return true;
+	}
+
+	void f6(const int x, const int y, std::stringstream& ss)
+	{
+		ss << std::format("{}\n{}", x, y);
+	}
 
 } // namespace
--- app/app.h
+++ app/app.h
@@ -3,11 +3,32 @@
 #include <string>
 #include <iostream>
 
-namespace vsite::oop::p1 
+namespace vsite::oop::p1
 {
-/*  
-* function declarations
-*/
+	/*
+	* function declarations
+	*/
+	double f1(const int x, const int y);
 
+	bool f2(const bool expression);
+
+	int f3(
+		const int a = 1,
+		const int b = 2,
+		const int c = 3,
+		const int d = 4
+	);
+
+	std::string f4(const bool element);
+	std::string f4(const int element);
+	std::string f4(const double element);
+
+	bool f5(std::string& s, const int num);
+
+	void f6(
+		const int x,
+		const int y,
+		std::stringstream& ss
+	);
 
 }
