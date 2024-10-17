bd07d75ae8425e4c0476c02824622c26426b21ac
breski-igor <igor.breski@vsite.hr>
2024-10-17 20:47:42 +0200

    p01

--- app/app.cpp
+++ app/app.cpp
@@ -3,10 +3,6 @@
 #include <format>
 
 namespace vsite::oop::p1 {
-
-/*
-* function implementations
-*/
 	
 	double f1(int a, int b) {
 		return std::pow(a,b);
--- app/app.h
+++ app/app.h
@@ -1,14 +1,9 @@
 #pragma once
-
 #include <string>
 #include <iostream>
 
 namespace vsite::oop::p1
 {
-	/*
-	* function declarations
-	*/
-
 	//f1
 	double f1(int a, int b);
 	
