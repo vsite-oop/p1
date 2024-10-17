8e7a411c7225bcd8fe8bc425c3f7bd5c61629c29
breski-igor <igor.breski@vsite.hr>
2024-10-17 11:35:43 +0200

    rijeseno p01

--- app/app.cpp
+++ app/app.cpp
@@ -1,5 +1,5 @@
 #include "app.h"
-
+#include <cmath>
 #include <format>
 
 namespace vsite::oop::p1 {
@@ -7,6 +7,46 @@ namespace vsite::oop::p1 {
 /*
 * function implementations
 */
+	
+	double f1(int a, int b) {
+		return std::pow(a,b);
+	};
+
+	bool f2(bool a) {
+		return (!a);
+	}
+
+	int f3(int a, int b, int c, int d) {
+		return a + b + c + d;
+	}
+
+	std::string f4(int) {
+		return "int";
+	}
+	std::string f4(bool) {
+		return "bool";
+	}
+	std::string f4(double) {
+		return "double";
+	}
+
+	bool f5(std::string& s, int a) {
+		if (a < 1) {
+			return false;
+		}
+		else {
+			int b = a % 10;
+			s += std::to_string(b);
+			return true;
+		}
+	
+	}
+
+	void f6(int a, int b, std::ostream& os)
+	{
+		os << a << '\n' << b;
+	}
+
 
 
 } // namespace
