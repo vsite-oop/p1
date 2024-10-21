7d20d72f9f2ecacc2286604cfc63920a5214f769
Ivan Ceranac <ivcerana@vsite.hr>
2024-10-21 15:04:40 +0200

    rijesenje prvog predavanja - testovi 1,2,3,6

--- app/app.cpp
+++ app/app.cpp
@@ -7,6 +7,36 @@ namespace vsite::oop::p1 {
 /*
 * function implementations
 */
+    double f1(double a, int n) {
+        // ako je eksponent 0 vrati 1
+        if (n == 0)
+            return 1.0;
 
+        double result = 1.0;
+
+        //koristim abs da rezultat dobijem za pozitivne i negativne eksponente
+        int exp = std::abs(n);
+
+        for (int i = 0; i < exp; ++i) {
+            result *= a;
+        }
+
+        // Ako je eksponent negativan vrati mi reciprocnu vrijednost
+        return (n > 0) ? result : 1.0 / result;
+    }
+
+    //vrati mi razlicito od true
+    bool f2(bool trueCond) {
+        return !trueCond;
+    }
+
+    int f3(int a, int b, int c, int d) {
+        return a+b+c+d;
+    }
+       
+
+    void f6(int a, int b, std::ostream& os) {
+        os << a << "\n" << b;
+    }
 
 } // namespace
--- app/app.h
+++ app/app.h
@@ -9,5 +9,9 @@ namespace vsite::oop::p1
 * function declarations
 */
 
+    double f1(double a, int n);
+    bool f2(bool trueCond);
+    int f3(int a = 1, int b = 2, int c = 3, int d = 4); // 4 param
+    void f6(int a, int b, std::ostream& os);
 
 }
