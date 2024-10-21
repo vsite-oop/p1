ad66a8ecc14e2bf6ae7b7fd30d692fbb0598beb2
Ivan Ceranac <ivcerana@vsite.hr>
2024-10-21 15:40:00 +0200

    riješenje p1 sa svim testovima

--- app/app.cpp
+++ app/app.cpp
@@ -33,7 +33,27 @@ namespace vsite::oop::p1 {
     int f3(int a, int b, int c, int d) {
         return a+b+c+d;
     }
-       
+    
+    std::string f4(int) {
+        return "int";
+    }
+
+    std::string f4(bool) {
+        return "bool";
+    }
+
+    std::string f4(double) {
+        return "double";
+    }
+
+    bool f5(std::string& s, int broj) {
+        if (broj <= 0) {
+            return false;
+        }
+
+        s += std::to_string(broj % 10);
+            return true;
+        }
 
     void f6(int a, int b, std::ostream& os) {
         os << a << "\n" << b;
--- app/app.h
+++ app/app.h
@@ -12,6 +12,10 @@ namespace vsite::oop::p1
     double f1(double a, int n);
     bool f2(bool trueCond);
     int f3(int a = 1, int b = 2, int c = 3, int d = 4); // 4 param
+    std::string f4(int);
+    std::string f4(bool);
+    std::string f4(double);
+    bool f5(std::string& s, const int broj);
     void f6(int a, int b, std::ostream& os);
 
 }
