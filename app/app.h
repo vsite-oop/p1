79253859deb72713237f7bb3523b3088d0aad2b6
rrasp94 <ruraspud@vsite.hr>
2024-11-04 14:39:13 +0100

    p01 solved

--- app/app.h
+++ app/app.h
@@ -8,6 +8,16 @@ namespace vsite::oop::p1
 /*  
 * function declarations
 */
+    double f1(int base, int exponent);
+    bool f2(bool condition);
+    int f3(int a = 1, int b = 2, int c = 3, int d = 4);
+    std::string f4(int);     
+    std::string f4(bool);     
+    std::string f4(double);   
+    bool f5(std::string& s, int n);
+    void f6(int a, int b, std::ostream& os);
+
+
 
 
 }
